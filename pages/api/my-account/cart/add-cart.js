7dcc28d5142208280c507f10ba2d4dd8b125e0e0
ghulam03 <ghulam8905@gmail.com>
2023-02-21 19:15:05 +0530

    commenting cart

--- pages/api/my-account/cart/add-cart.js
+++ pages/api/my-account/cart/add-cart.js
@@ -1,25 +1,25 @@
-import prisma from "../../../prisma/prisma";
+// import prisma from "../../../prisma/prisma";
 
-export default async function handle(req, res) {
-  await prisma.$connect();
-  if (req.method === "POST") {
-    const { id, tprice, titem, uId, products } = req.body;
+// export default async function handle(req, res) {
+//   await prisma.$connect();
+//   if (req.method === "POST") {
+//     const { id, tprice, titem, uId, products } = req.body;
 
-    console.log(id, "will be added to cart");
+//     console.log(id, "will be added to cart");
 
-    const rest = await prisma.cart.create({
-      data: {
-        id,
-        tprice,
-        titem,
-        uId,
-        products: {
-          create: {
-            ...products,
-          },
-        },
-      },
-    });
-    res.json(rest);
-  }
-}
+//     const rest = await prisma.cart.create({
+//       data: {
+//         id,
+//         tprice,
+//         titem,
+//         uId,
+//         products: {
+//           create: {
+//             ...products,
+//           },
+//         },
+//       },
+//     });
+//     res.json(rest);
+//   }
+// }
--- pages/api/my-account/cart/get-cart.js
+++ pages/api/my-account/cart/get-cart.js
@@ -1,27 +1,27 @@
-import prisma from "../../../prisma/prisma";
+// import prisma from "../../../prisma/prisma";
 
-export default async function handle(req, res) {
-  await prisma.$connect();
+// export default async function handle(req, res) {
+//   await prisma.$connect();
 
-  const { uId } = req.body;
-  // console.log( id, "will be added to cart");
+//   const { uId } = req.body;
+//   // console.log( id, "will be added to cart");
 
-  const rest = await prisma.cart.findMany({
-    where: {
-      uId,
-    },
-    select: {
-      pcode: true,
-    },
-  });
-  console.log(rest);
-  const data = await prisma.product.findMany({
-    where: {
-      pcode: {
-        in: rest,
-      },
-    },
-  });
+//   const rest = await prisma.cart.findMany({
+//     where: {
+//       uId,
+//     },
+//     select: {
+//       pcode: true,
+//     },
+//   });
+//   console.log(rest);
+//   const data = await prisma.product.findMany({
+//     where: {
+//       pcode: {
+//         in: rest,
+//       },
+//     },
+//   });
 
-  // res.json(rest);
-}
+//   // res.json(rest);
+// }
